d9329691c420968abdf7788ba82d05e18e6994e4
galyniayaroshenko <yaroshenko270@gmail.com>
2016-02-21 21:53:38 +0200

    Modification

--- app/js/controllers/newsViewController.js
+++ app/js/controllers/newsViewController.js
@@ -1,7 +1,11 @@
 angular.module('myApp')
-  .controller('newsViewController', ['$scope','$stateParams', 'news', function($scope, $stateParams, news) {
+  .controller('newsViewController', ['$scope','$stateParams', 'news', '$sce',
+    function($scope, $stateParams, news , $sce) {
     console.log($stateParams.id);
-    $scope.news = news.get({ action: $stateParams.id });
-    console.log('ew', $scope.news);
-    console.log('smile');
+      news.get({ action: $stateParams.id }, function(data) {
+        $scope.news = data;
+        $scope.news.description1 = $sce.trustAsHtml($scope.news.description1);
+        console.log('ew', $scope.news);
+        console.log('smile');
+    });
   }]);
